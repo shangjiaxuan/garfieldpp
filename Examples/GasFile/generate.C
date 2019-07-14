abc9adde8d15625a435d119eaa0fe934c6335f50
hschindl <heinrich.schindler@cern.ch>
2019-07-14 15:00:28 +0200

    Create MediumMagboltz object on the stack (to be consistent with the example on the webpage).

--- Examples/GasFile/generate.C
+++ Examples/GasFile/generate.C
@@ -17,24 +17,24 @@ int main(int argc, char * argv[]) {
   const double temperature = 293.15;
  
   // Setup the gas.
-  MediumMagboltz* gas = new MediumMagboltz();
-  gas->SetTemperature(temperature);
-  gas->SetPressure(pressure);
-  gas->SetComposition("Ar", 93., "CO2", 7.);
- 
+  MediumMagboltz gas;
+  gas.SetComposition("Ar", 93., "CO2", 7.);
+  gas.SetTemperature(temperature);
+  gas.SetPressure(pressure);
+
   // Set the field range to be covered by the gas table. 
   const int nFields = 20;
   const double emin =    100.;
   const double emax = 100000.;
   // Flag to request logarithmic spacing.
-  const bool useLog = true;
-  gas->SetFieldGrid(emin, emax, nFields, useLog); 
+  constexpr bool useLog = true;
+  gas.SetFieldGrid(emin, emax, nFields, useLog); 
 
   const int ncoll = 10;
   // Run Magboltz to generate the gas table.
-  gas->GenerateGasTable(ncoll);
+  gas.GenerateGasTable(ncoll);
   // Save the table. 
-  gas->WriteGasFile("ar_93_co2_7.gas");
+  gas.WriteGasFile("ar_93_co2_7.gas");
 
   // app.Run(kTRUE);
 
