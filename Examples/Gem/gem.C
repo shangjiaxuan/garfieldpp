6cf90898b2f22a8595ac65bff65c7a94ce0bf316
hschindl <heinrich.schindler@cern.ch>
2019-08-09 15:43:40 +0200

    Don't plot the field inside the kapton.

--- Examples/Gem/gem.C
+++ Examples/Gem/gem.C
@@ -40,6 +40,7 @@ int main(int argc, char * argv[]) {
     fieldView->SetPlane(0., -1., 0., 0., 0., 0.);
     fieldView->SetArea(-pitch / 2., -0.02, pitch / 2., 0.02);
     fieldView->SetVoltageRange(-160., 160.);
+    fieldView->EnableAcknowledgeStatus();
     TCanvas* cf = new TCanvas("cf", "", 600, 600);
     cf->SetLeftMargin(0.16);
     fieldView->SetCanvas(cf);
