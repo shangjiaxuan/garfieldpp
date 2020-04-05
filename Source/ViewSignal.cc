2f7449a6ca5180e11b4c16a2cd83ae2366f75cfd
hschindl <heinrich.schindler@cern.ch>
2020-04-05 20:30:03 +0200

    Use DrawCopy

--- Source/ViewSignal.cc
+++ Source/ViewSignal.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <TGraph.h>
 #include <TAxis.h>
 
 #include "Garfield/GarfieldConstants.hh"
@@ -58,7 +59,6 @@ void ViewSignal::PlotSignal(const std::string& label, const bool total,
   m_sensor->GetTimeWindow(t0, dt, nBins);
   const double t1 = t0 + nBins * dt;
 
-  const auto title = label.c_str();
   std::string xlabel = "time [ns]"; 
   std::string ylabel = m_labelY;
   if (ylabel.empty()) {
@@ -66,7 +66,7 @@ void ViewSignal::PlotSignal(const std::string& label, const bool total,
   }
   if (total) {
     auto hname = FindUnusedHistogramName("hSignal_").c_str();
-    m_hSignal.reset(new TH1D(hname, title, nBins, t0, t1));
+    m_hSignal.reset(new TH1D(hname, "", nBins, t0, t1));
     m_hSignal->SetLineColor(plottingEngine.GetRootColorLine1());
     m_hSignal->GetXaxis()->SetTitle(xlabel.c_str()),
     m_hSignal->GetYaxis()->SetTitle(ylabel.c_str());
@@ -75,29 +75,32 @@ void ViewSignal::PlotSignal(const std::string& label, const bool total,
       const double sig = m_sensor->GetSignal(label, i);
       m_hSignal->SetBinContent(i + 1, sig);
     }
-    m_hSignal->Draw("");
+    m_hSignal->DrawCopy("");
     if (m_userRangeX) m_hSignal->SetAxisRange(m_xmin, m_xmax, "X");
     if (m_userRangeY) m_hSignal->SetAxisRange(m_ymin, m_ymax, "Y");
 
     // Get and plot threshold crossings.
     const auto nCrossings = m_sensor->GetNumberOfThresholdCrossings();
     if (nCrossings > 0) {
-      m_gCrossings.reset(new TGraph(nCrossings));
-      m_gCrossings->SetMarkerStyle(20);
-      m_gCrossings->SetMarkerColor(plottingEngine.GetRootColorLine1());
+      TGraph gCrossings;
+      gCrossings.SetMarkerStyle(20);
+      gCrossings.SetMarkerColor(plottingEngine.GetRootColorLine1());
+      std::vector<double> xp;
+      std::vector<double> yp;
       double time = 0., level = 0.;
       bool rise = true;
       for (unsigned int i = 0; i < nCrossings; ++i) {
         if (m_sensor->GetThresholdCrossing(i, time, level, rise)) {
-          m_gCrossings->SetPoint(i, time, level);
+          xp.push_back(time);
+          yp.push_back(level);
         }
       }
-      m_gCrossings->Draw("psame");
+      gCrossings.DrawGraph(xp.size(), xp.data(), yp.data(), "psame");
     } 
 
     if (delayed) {
       hname = FindUnusedHistogramName("hDelayedSignal_").c_str();
-      m_hDelayedSignal.reset(new TH1D(hname, title, nBins, t0, t1));
+      m_hDelayedSignal.reset(new TH1D(hname, "", nBins, t0, t1));
       m_hDelayedSignal->SetLineColor(kCyan + 2);
       m_hDelayedSignal->SetLineStyle(2);
       m_hDelayedSignal->SetStats(0);
@@ -106,7 +109,7 @@ void ViewSignal::PlotSignal(const std::string& label, const bool total,
                            m_sensor->GetDelayedIonSignal(label, i);
         m_hDelayedSignal->SetBinContent(i + 1, sig);
       }
-      m_hDelayedSignal->Draw("same");
+      m_hDelayedSignal->DrawCopy("same");
     }
     gPad->Update();
   }
@@ -114,7 +117,7 @@ void ViewSignal::PlotSignal(const std::string& label, const bool total,
   // Plot the electron and ion signals if requested.
   if (electron) {
     auto hname = FindUnusedHistogramName("hSignalElectrons_").c_str();
-    m_hSignalElectrons.reset(new TH1D(hname, title, nBins, t0, t1));
+    m_hSignalElectrons.reset(new TH1D(hname, "", nBins, t0, t1));
     m_hSignalElectrons->SetLineColor(plottingEngine.GetRootColorElectron());
     m_hSignalElectrons->GetXaxis()->SetTitle(xlabel.c_str());
     m_hSignalElectrons->GetYaxis()->SetTitle(ylabel.c_str());
@@ -123,14 +126,14 @@ void ViewSignal::PlotSignal(const std::string& label, const bool total,
       const double sig = m_sensor->GetElectronSignal(label, i);
       m_hSignalElectrons->SetBinContent(i + 1, sig);
     }
-    m_hSignalElectrons->Draw("same");
+    m_hSignalElectrons->DrawCopy("same");
     if (!total) {
       if (m_userRangeX) m_hSignalElectrons->SetAxisRange(m_xmin, m_xmax, "X");
       if (m_userRangeY) m_hSignalElectrons->SetAxisRange(m_ymin, m_ymax, "Y");
     }
     if (delayed) {
       hname = FindUnusedHistogramName("hDelayedSignalElectrons_").c_str();
-      m_hDelayedSignalElectrons.reset(new TH1D(hname, title, nBins, t0, t1));
+      m_hDelayedSignalElectrons.reset(new TH1D(hname, "", nBins, t0, t1));
       m_hDelayedSignalElectrons->SetLineColor(kYellow - 7);
       m_hDelayedSignalElectrons->SetLineStyle(2);
       m_hDelayedSignalElectrons->SetStats(0);
@@ -138,13 +141,13 @@ void ViewSignal::PlotSignal(const std::string& label, const bool total,
         const double sig = m_sensor->GetDelayedElectronSignal(label, i);
         m_hDelayedSignalElectrons->SetBinContent(i + 1, sig);
       }
-      m_hDelayedSignalElectrons->Draw("same");
+      m_hDelayedSignalElectrons->DrawCopy("same");
     }
     gPad->Update();
   }
   if (ion) {
     auto hname = FindUnusedHistogramName("hSignalIons_").c_str();
-    m_hSignalIons.reset(new TH1D(hname, title, nBins, t0, t1));
+    m_hSignalIons.reset(new TH1D(hname, "", nBins, t0, t1));
     m_hSignalIons->SetLineColor(plottingEngine.GetRootColorIon());
     m_hSignalIons->GetXaxis()->SetTitle(xlabel.c_str());
     m_hSignalIons->GetYaxis()->SetTitle(ylabel.c_str());
@@ -153,14 +156,14 @@ void ViewSignal::PlotSignal(const std::string& label, const bool total,
       const double sig = m_sensor->GetIonSignal(label, i);
       m_hSignalIons->SetBinContent(i + 1, sig);
     }
-    m_hSignalIons->Draw("same");
+    m_hSignalIons->DrawCopy("same");
     if (!(total || electron)) {
       if (m_userRangeX) m_hSignalIons->SetAxisRange(m_xmin, m_xmax, "X");
       if (m_userRangeY) m_hSignalIons->SetAxisRange(m_ymin, m_ymax, "Y");
     }
     if (delayed) {
       hname = FindUnusedHistogramName("hDelayedSignalIons_").c_str();
-      m_hDelayedSignalIons.reset(new TH1D(hname, title, nBins, t0, t1));
+      m_hDelayedSignalIons.reset(new TH1D(hname, "", nBins, t0, t1));
       m_hDelayedSignalIons->SetLineColor(kRed - 9);
       m_hDelayedSignalIons->SetLineStyle(2);
       m_hDelayedSignalIons->SetStats(0);
@@ -168,7 +171,7 @@ void ViewSignal::PlotSignal(const std::string& label, const bool total,
         const double sig = m_sensor->GetDelayedIonSignal(label, i);
         m_hDelayedSignalIons->SetBinContent(i + 1, sig);
       }
-      m_hDelayedSignalIons->Draw("same");
+      m_hDelayedSignalIons->DrawCopy("same");
     }
     gPad->Update();
   }
