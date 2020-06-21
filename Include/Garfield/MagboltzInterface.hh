9cf9533cd69edf24eec1beafb70d9c2959c95c22
hschindl <heinrich.schindler@cern.ch>
2020-06-21 18:42:45 +0200

    Add common blocks used with thermal-motion flag switched on. Fix variable type of icoln. Convert collision counters to rates.

--- Include/Garfield/MagboltzInterface.hh
+++ Include/Garfield/MagboltzInterface.hh
@@ -17,6 +17,7 @@ constexpr unsigned int nMaxNullTerms = 10;
 constexpr unsigned int nMaxLevelsPerComponent = 300;
 constexpr unsigned int nCharDescr = 50;
 constexpr unsigned int nMaxLevels = 960;
+constexpr unsigned int nMaxComponents = 6;
 
 extern "C" {
 
@@ -93,14 +94,36 @@ extern struct {
   double z;
   double st;
   long long nnull;
-  double icoln[nMaxLevels];
+  long long icoln[nMaxLevels];
   long long icolnn[60];
 } outpt_;
+
+extern struct {
+  double time[300];
+  long long icoll[5][nMaxComponents];
+  double spec[nEnergySteps];
+  double tmax1;
+  double ave;
+  double den;
+  double xid;
+  double x;
+  double y;
+  double z;
+  double st;
+  long long nnull;
+  long long icoln[290][nMaxComponents];
+  long long icolnn[10][nMaxComponents];
+} outptt_;
 
 extern struct {
   char dscrpt[nMaxLevels][nCharDescr];
   char dscrptn[60][nCharDescr];
 } scrip_;
+
+extern struct {
+  char dscrpt[nMaxLevelsPerComponent][nMaxComponents][nCharDescr];
+  char dscrptn[10][nMaxComponents][nCharDescr];
+} script_;
 
 extern struct {
   double cf[nMaxLevels][nEnergySteps];
@@ -115,6 +138,20 @@ extern struct {
   double penfra[nMaxLevels][3];
   double tcfmax[8];
 } large_;
+
+extern struct {
+  double cf[290][nEnergySteps][nMaxComponents];
+  double ein[290][nMaxComponents];
+  double tcf[nEnergySteps][nMaxComponents];
+  long long iarry[290][nMaxComponents];
+  double rgas[290][nMaxComponents];
+  double ipn[290][nMaxComponents];
+  double wpl[290][nMaxComponents];
+  long long last[nMaxComponents];
+  long long isize[nMaxComponents];
+  double penfra[290][3][nMaxComponents];
+  double tcfmax[nMaxComponents];
+} larget_;
 
 // Definition of the gas mixture
 extern struct { long long ngasn[6]; } gasn_;
@@ -161,6 +198,12 @@ void gasmix_(long long* ngs, double* q, double* qin, long long* nin, double* e,
              long long* ng1, double* eg1, long long* ng2, double* eg2,
              char scrpt[nMaxLevelsPerComponent][nCharDescr],
              char scrptn[nMaxNullTerms][nCharDescr]);
+
+void colf_(double* freq, double* freel, double* freion, double* freatt, 
+           double* frein, long long *ntotal);
+
+void colft_(double* freq, double* freel, double* freion, double* freatt, 
+            double* frein, long long *ntotal);
 
 void magboltz_();
 }
