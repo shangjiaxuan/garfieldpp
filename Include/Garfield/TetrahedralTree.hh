c17315a50aab7ee9f69f9718f37c3ae2aa9a69a4
hschindl <heinrich.schindler@cern.ch>
2020-10-11 11:41:09 +0200

    Not all members of a class are initialized inside the constructor. Consider inspecting: x, y, z.

--- Include/Garfield/TetrahedralTree.hh
+++ Include/Garfield/TetrahedralTree.hh
@@ -10,7 +10,7 @@ namespace Garfield {
 // TODO: replace this class with ROOT's TVector3 class
 
 struct Vec3 {
-  float x, y, z;
+  float x = 0., y = 0., z = 0.;
 
   Vec3() {}
   Vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
