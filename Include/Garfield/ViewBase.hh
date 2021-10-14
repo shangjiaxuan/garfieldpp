29e5a25896af7e7d3896a2eb02231cc2fb8403c8
hschindl <heinrich.schindler@cern.ch>
2021-10-14 14:48:18 +0200

    typo

--- Include/Garfield/ViewBase.hh
+++ Include/Garfield/ViewBase.hh
@@ -144,9 +144,9 @@ class ViewBase {
                   double& xmin, double& ymin,
                   double& xmax, double& ymax) const;
 
-  bool RangeSet(TPad*);
-  void SetRange(TPad* pad, const double x0, const double y0,
-                const double x1, const double y1);
+  static bool RangeSet(TVirtualPad*);
+  static void SetRange(TVirtualPad* pad, const double x0, const double y0,
+                       const double x1, const double y1);
  private:
   // Current pad.
   TPad* m_pad = nullptr;
