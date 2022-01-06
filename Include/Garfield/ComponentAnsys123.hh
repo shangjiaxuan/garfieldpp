c82f9944ee9fa841fbe5bdbd7f395b47498f2656
hschindl <heinrich.schindler@cern.ch>
2022-01-06 21:32:19 +0100

    Move functions ElectricField, WeightingField, WeightingPotential, GetMedium from ComponentAnsys123, ComponentComsol, ComponentElmer to ComponentFieldMap.

--- Include/Garfield/ComponentAnsys123.hh
+++ Include/Garfield/ComponentAnsys123.hh
@@ -14,21 +14,6 @@ class ComponentAnsys123 : public ComponentFieldMap {
   /// Destructor
   ~ComponentAnsys123() {}
 
-  void ElectricField(const double x, const double y, const double z, double& ex,
-                     double& ey, double& ez, Medium*& m, int& status) override;
-  void ElectricField(const double x, const double y, const double z, double& ex,
-                     double& ey, double& ez, double& v, Medium*& m,
-                     int& status) override;
-
-  void WeightingField(const double x, const double y, const double z,
-                      double& wx, double& wy, double& wz,
-                      const std::string& label) override;
-
-  double WeightingPotential(const double x, const double y, const double z,
-                            const std::string& label) override;
-
-  Medium* GetMedium(const double x, const double y, const double z) override;
-
   bool Initialise(std::string elist = "ELIST.lis",
                   std::string nlist = "NLIST.lis",
                   std::string mplist = "MPLIST.lis",
