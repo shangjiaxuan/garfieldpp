af4a14516489babbd6e6df780edd58fedf7fa12b
hschindl <heinrich.schindler@cern.ch>
2022-02-21 11:55:37 +0100

    Plot the mesh on top of the equipotential contour lines.

--- Examples/Gem/gem.C
+++ Examples/Gem/gem.C
@@ -21,31 +21,6 @@ int main(int argc, char * argv[]) {
 
   TApplication app("app", &argc, argv);
 
-  // Load the field map.
-  ComponentAnsys123 fm;
-  fm.Initialise("ELIST.lis", "NLIST.lis", "MPLIST.lis", "PRNSOL.lis", "mm");
-  fm.EnableMirrorPeriodicityX();
-  fm.EnableMirrorPeriodicityY();
-  fm.PrintRange();
-
-  // Dimensions of the GEM [cm]
-  constexpr double pitch = 0.014;
-
-  ViewField fieldView;
-  constexpr bool plotField = true;
-  if (plotField) {
-    fieldView.SetComponent(&fm);
-    // Set the normal vector of the viewing plane (xz plane).
-    fieldView.SetPlane(0, -1, 0, 0, 0, 0);
-    // Set the plot limits in the current viewing plane.
-    fieldView.SetArea(-0.5 * pitch, -0.02, 0.5 * pitch, 0.02);
-    fieldView.SetVoltageRange(-160., 160.);
-    TCanvas* cf = new TCanvas("cf", "", 600, 600);
-    cf->SetLeftMargin(0.16);
-    fieldView.SetCanvas(cf);
-    fieldView.PlotContour();
-  }
-
   // Setup the gas.
   MediumMagboltz gas;
   gas.SetComposition("ar", 80., "co2", 20.);
@@ -59,6 +34,14 @@ int main(int argc, char * argv[]) {
   // Load the ion mobilities.
   const std::string path = std::getenv("GARFIELD_INSTALL");
   gas.LoadIonMobility(path + "/share/Garfield/Data/IonMobility_Ar+_Ar.txt");
+
+  // Load the field map.
+  ComponentAnsys123 fm;
+  fm.Initialise("ELIST.lis", "NLIST.lis", "MPLIST.lis", "PRNSOL.lis", "mm");
+  fm.EnableMirrorPeriodicityX();
+  fm.EnableMirrorPeriodicityY();
+  fm.PrintRange();
+
   // Associate the gas with the corresponding field map material. 
   const size_t nMaterials = fm.GetNumberOfMaterials();
   for (size_t i = 0; i < nMaterials; ++i) {
@@ -66,6 +49,33 @@ int main(int argc, char * argv[]) {
   }
   fm.PrintMaterials();
   // fm.Check();
+
+  // Dimensions of the GEM [cm]
+  constexpr double pitch = 0.014;
+
+  ViewField fieldView;
+  ViewFEMesh meshView;
+  constexpr bool plotField = true;
+  if (plotField) {
+    fieldView.SetComponent(&fm);
+    // Set the normal vector of the viewing plane (xz plane).
+    fieldView.SetPlane(0, -1, 0, 0, 0, 0);
+    // Set the plot limits in the current viewing plane.
+    fieldView.SetArea(-0.5 * pitch, -0.02, 0.5 * pitch, 0.02);
+    fieldView.SetVoltageRange(-160., 160.);
+    TCanvas* cf = new TCanvas("cf", "", 600, 600);
+    cf->SetLeftMargin(0.16);
+    fieldView.SetCanvas(cf);
+    fieldView.PlotContour();
+
+    meshView.SetArea(-0.5 * pitch, -0.02, 0.5 * pitch, 0.02); 
+    meshView.SetCanvas(cf);
+    meshView.SetComponent(&fm);
+    meshView.SetPlane(0, -1, 0, 0, 0, 0);
+    meshView.SetFillMesh(true);
+    meshView.SetColor(2, kGray);
+    meshView.Plot(true);
+  }
 
   // Create the sensor.
   Sensor sensor;
@@ -117,24 +127,23 @@ int main(int argc, char * argv[]) {
     TCanvas* cd = new TCanvas();
     constexpr bool plotMesh = true;
     if (plotMesh) {
-      ViewFEMesh* meshView = new ViewFEMesh();
-      meshView->SetArea(-2 * pitch, -2 * pitch, -0.02, 
-                         2 * pitch,  2 * pitch, 0.02);
-      meshView->SetCanvas(cd);
-      meshView->SetComponent(&fm);
-      // x-z projection.
-      meshView->SetPlane(0, -1, 0, 0, 0, 0);
-      meshView->SetFillMesh(true);
-      meshView->SetColor(0, kGray);
-      // Set the color of the kapton.
-      meshView->SetColor(2, kYellow + 3);
-      meshView->EnableAxes();
-      meshView->SetViewDrift(&driftView);
+      meshView.SetCanvas(cd);
+      meshView.SetComponent(&fm);
       constexpr bool twod = true;
-      if (!twod) {
-        meshView->SetArea(-0.5 * pitch, -0.5 * pitch, -0.02, 0.5 * pitch, 0.5 * pitch, 0.02);
+      // x-z projection.
+      meshView.SetPlane(0, -1, 0, 0, 0, 0);
+      if (twod) {
+        meshView.SetArea(-2 * pitch, -0.02, 2 * pitch, 0.02);
+      } else {
+        meshView.SetArea(-0.5 * pitch, -0.5 * pitch, -0.02, 0.5 * pitch, 0.5 * pitch, 0.02);
       }
-      meshView->Plot(twod);
+      meshView.SetFillMesh(true);
+      meshView.SetColor(0, kGray);
+      // Set the color of the kapton.
+      meshView.SetColor(2, kYellow + 3);
+      meshView.EnableAxes();
+      meshView.SetViewDrift(&driftView);
+      meshView.Plot(twod);
     } else {
       driftView.SetPlane(0, -1, 0, 0, 0, 0);
       driftView.SetArea(-2 * pitch, -0.02, 2 * pitch, 0.02);
